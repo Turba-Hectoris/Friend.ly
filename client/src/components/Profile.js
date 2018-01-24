08082de14cf078caf5a522cc2604883e0a4751d1
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2018-01-24 18:56:53 -0500

    add stylesheet for grid & init edit on profile component

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -3,8 +3,26 @@ import React from 'react';
 export class Profile extends React.Component {
   render() {
     return (
-      <div>
-        Profile
+      <div className="lt
+        lt-xs-x-1
+        lt-xs-y-1
+        lt-xs-w-1
+        lt-xs-h-1
+        lt-sm-x-0
+        lt-sm-y-1
+        lt-sm-w-2
+        lt-sm-h-2
+        lt-md-x-0
+        lt-md-y-1
+        lt-md-w-4
+        lt-md-h-2
+        lt-lg-x-0
+        lt-lg-y-1
+        lt-lg-w-4
+        lt-lg-h-2">
+          <div className="lt-body">
+            
+          </div>
       </div>
     );
   }
