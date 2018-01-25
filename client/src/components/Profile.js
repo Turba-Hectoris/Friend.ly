bf33415d7902135605d6188893db9c3c86d4b97f
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2018-01-25 14:07:18 -0500

    add responsive classes to profile component

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -17,16 +17,36 @@ export class Profile extends React.Component {
         lt-md-w-4
         lt-md-h-2
         lt-lg-x-0
-        lt-lg-y-1
-        lt-lg-w-4
-        lt-lg-h-2">
-          <div className="lt-body">
-            <div className="profile-view">
-              <div className="profile-image-box">
-                <img src="stock-user-profile" alt="" className="profile-image"/>
-              </div>
-            </div>
+        lt-lg-y-0
+        lt-lg-w-5
+        lt-lg-h-5">
+        <div className="lt-body">
+          <div className="profile-body">  
+            <div className="lt
+              lt-xs-x-1
+              lt-xs-y-1
+              lt-xs-w-1
+              lt-xs-h-1
+              lt-sm-x-0
+              lt-sm-y-1
+              lt-sm-w-2
+              lt-sm-h-2
+              lt-md-x-0
+              lt-md-y-1
+              lt-md-w-4
+              lt-md-h-2
+              lt-lg-x-1
+              lt-lg-y-1
+              lt-lg-w-1
+              lt-lg-h-1">
+                <div className="lt-body">
+                  <div className="profile-image-div">
+                    <img src="stock-user-profile.jpg" alt="" className="profile-image"/>
+                  </div>  
+                </div> 
+            </div>   
           </div>
+        </div>
       </div>
     );
   }
