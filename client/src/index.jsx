a36ff1ff65d9b57809f634f1b8ce4edfa176b7c5
ac-2017 <exceedhero@gmail.com>
2018-01-24 20:06:21 -0500

    Landing page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 // import AnyComponent from './components/filename.jsx'
+import Homepage from './components/Homepage.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +13,14 @@ class App extends React.Component {
   }
 
   render () {
-    return (<div>Hello World</div>)
+    return (
+      <div style={{width:'100%', height:'100%'}}>
+      {
+        //Don't modify unless you're aaron
+      /*<header className="nav"><span className='logo' style={{marginLeft:'10%'}}>Friend.ly</span><ul><li>Events</li><li>Profile</li></ul></header>
+      <Homepage/>*/}
+      </div>
+      )
   }
 }
 
