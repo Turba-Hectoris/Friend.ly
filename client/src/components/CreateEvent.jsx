736c99102af4d46ed87c03e8988d0b692aff4217
William Eichhold <wjeichhold@gmail.com>
2018-02-06 18:56:03 -0500

    google map added to create event

--- client/src/components/CreateEvent.jsx
+++ client/src/components/CreateEvent.jsx
@@ -1,6 +1,7 @@
-import 'react-dates/initialize'; 
 import React from 'react';
 import axios from 'axios';
+import 'react-dates/initialize'; 
+import { DateRangePicker, DayPickerRangeController } from 'react-dates';
 import {
     Form,
     StyledText,
@@ -10,72 +11,84 @@ import {
     StyledSelect,
     StyledCheckbox
   } from 'react-form';
-import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import CreateMap from './CreateMap.jsx';
 
-    let categories = [
-	    {
-	      label: 'Movies',
-	      value: 'movies'
-	    },
-	    {
-	      label: 'Outdoors',
-	      value: 'outdoors'
-	    },
-	    {
-	      label: 'Food/Dining',
-	      value: 'food'
-	    },
-	    {
-	    	label: 'Live Music',
-	    	value: 'music'
-	    },
-	    {
-	    	label: 'Exercise',
-	    	value: 'exercise'
-	    },
-	    {
-	    	label: 'Gaming',
-	    	value: 'gaming'
-	    },
-	    {
-	    	label: 'Drinks',
-	    	value: 'drinks'
-	    },
-	    {
-	    	label: 'Arts & Culture',
-	    	value: 'arts'
-	    }
-  	]
+    let categories = [ //Categories that the user can choose when creating an event
+      {
+        label: 'Movies',
+        value: 'movies'
+      },
+      {
+        label: 'Outdoors',
+        value: 'outdoors'
+      },
+      {
+        label: 'Food/Dining',
+        value: 'food'
+      },
+      {
+        label: 'Live Music',
+        value: 'music'
+      },
+      {
+        label: 'Exercise',
+        value: 'exercise'
+      },
+      {
+        label: 'Gaming',
+        value: 'gaming'
+      },
+      {
+        label: 'Drinks',
+        value: 'drinks'
+      },
+      {
+        label: 'Arts & Culture',
+        value: 'arts'
+      }
+    ]
 
   let capacity = [
-  	{
-  		label: '1',
-  		value: '1'
-  	},
-  	{
-  		label: '2',
-  		value: '2'
-  	},
-  	{
-  		label: '3',
-  		value: '3'
-  	},
-  	{
-  		label: '4',
-  		value: '4'
-  	}
+    {
+      label: '1',
+      value: '1'
+    },
+    {
+      label: '2',
+      value: '2'
+    },
+    {
+      label: '3',
+      value: '3'
+    },
+    {
+      label: '4',
+      value: '4'
+    }
   ]
 
 class CreateEvent extends React.Component{
-	constructor(props) {
+  constructor(props) {
     super(props)
     this.state = {
-      submittedValues: {}
+      submittedValues: {},
+      locale: '',
+      location: ''
     }
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.setLocale = this.setLocale.bind(this);
   }
 
-  componentDidMount() {
-    console.log(this.props)
+  handleLocationChange (location) {
+    this.setState({
+      location: location
+    });
+  }
+
+  setLocale(locale) {
+    this.setState({
+      locale: locale
+    })
   }
 
     // errorValidator( values ){
@@ -159,20 +172,22 @@ class CreateEvent extends React.Component{
 
   render() {
 
-  	return(    
-  		<Form
+    return(
+      <div>
+      <Form
       // validateError={this.errorValidator}
       // validateWarning={this.warningValidator}
       // validateSuccess={this.successValidator}
+
       onSubmit={submittedValues => this.setState({submittedValues: submittedValues}, () => {axios.post('/createEvent', {
-      	eventName: this.state.submittedValues.eventName,
-      	eventDesc: this.state.submittedValues.eventDesc,
-      	capacity: this.state.submittedValues.capacity,
-      	category: this.state.submittedValues.category,
+        eventName: this.state.submittedValues.eventName,
+        eventDesc: this.state.submittedValues.eventDesc,
+        capacity: this.state.submittedValues.capacity,
+        category: this.state.submittedValues.category,
         creatorID: this.props.userID,
         startDate: this.state.startDate,
         endDate: this.state.endDate
-      	})})
+        })})
       .then((response) => {console.log(response)})}>
       { formApi => (
         <form onSubmit={formApi.submitForm} id="form2">
@@ -186,22 +201,21 @@ class CreateEvent extends React.Component{
           <StyledSelect field="category" id="category" options={categories} />
           <StyledCheckbox field="authorize" id="authorize" label="Authorize" className="d-inline-block" />
           <DateRangePicker
-  startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-  startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-  endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-  endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-  onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-  focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-  onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-/>
-
-
-
+            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+          />
           <button type="submit" className="mb-4 btn btn-primary">Submit</button>
         </form>
       )}
     </Form>
-  		)
+    <CreateMap getEventCoordinate={this.handleLocationChange} setLocale={this.setLocale}/>
+    </div>
+      )
   }
 }
 
