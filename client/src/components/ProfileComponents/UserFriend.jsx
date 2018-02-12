2456d43d063c6764e7da591e954c9a35335721ba
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2018-02-12 11:50:18 -0500

    profile grid styling

--- client/src/components/ProfileComponents/UserFriend.jsx
+++ client/src/components/ProfileComponents/UserFriend.jsx
@@ -7,12 +7,12 @@ export const UserFriend = ({ getUserDisplayedData, friend, handleAddFriend, hand
          <div className="profile_friend_info_container" >
           <Link className="profile_friend_image" onClick={() => {getUserDisplayedData(friend.userID)}} to={`/profile/${friend.userID}`}>
               <img className="" src={`${friend.profilePic}`} alt=""/>
-              {
-                displayedUser ? <button className="btn add_friend_button" type='button' onClick={() => {handleAddFriend(friend.userID)}} > Add Friend </button> :
-                <button className="btn add_friend_button" type='button' onClick={() => {handleUnfriend(friend.userID)}} > Remove Friend </button>
-              }
               <div className="profile_friend_username">{friend.username}</div>
           </Link>
+          {
+            displayedUser ? <button className="btn add_friend_button" type='button' onClick={() => {handleAddFriend(friend.userID)}} > Add Friend </button> :
+            <button className="btn add_friend_button" type='button' onClick={() => {handleUnfriend(friend.userID)}} > Remove Friend </button>
+          }
           <p className="profile_friend_bio">{friend.bio}</p>
           <hr/>
           <div className="profile_friend_email">{friend.email}</div>
